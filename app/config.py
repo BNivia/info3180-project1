06694a3a118cb7194929c8b8afc1c27e9fc62dd2
BNivia <nobenjamin19@gmail.com>
2021-03-14 23:30:01 -0500

    Updating driver.

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://bjkryickfgkant:4c71332dcd5c50bf78c4a2cc9cf0270fd552a8467436cdad31aa8c488905cd76@ec2-52-71-161-140.compute-1.amazonaws.com:5432/d2kvfu29f9o9g7'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql+psycopg2://bjkryickfgkant:4c71332dcd5c50bf78c4a2cc9cf0270fd552a8467436cdad31aa8c488905cd76@ec2-52-71-161-140.compute-1.amazonaws.com:5432/d2kvfu29f9o9g7'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = './uploads'
     
