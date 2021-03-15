e867de8837538e7776361bd49c3b88189fc545eb
BNivia <nobenjamin19@gmail.com>
2021-03-14 23:46:48 -0500

    Change to config.

--- app/config.py
+++ app/config.py
@@ -4,10 +4,9 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql+psycopg2://bjkryickfgkant:4c71332dcd5c50bf78c4a2cc9cf0270fd552a8467436cdad31aa8c488905cd76@ec2-52-71-161-140.compute-1.amazonaws.com:5432/d2kvfu29f9o9g7'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
+    #SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = './uploads'
-    
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
     DEVELOPMENT = True
